20c94eb15a252d55b95a600b42098606e781b00a
VictorGiuliano <victorgiuliano98@gmail.com>
2023-03-08 18:50:39 +0100

    Fix css button card

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -10,11 +10,11 @@
             <img src='{{$project->image}}' alt="{{$project->title}}">
             <h4>Title: {{$project->title}}</h4>
             <div class="container d-flex justify-content-center">
-                <a href="{{route('admin.projects.show', $project->id)}}" class="btn mx-1 small btn-primary"><i class="fa-solid fa-eye"></i></a>
+                <a href="{{route('admin.projects.show', $project->id)}}" class="btn mx-3 small btn-primary"><i class="fa-solid fa-eye"></i></a>
                 <form action="{{route('admin.projects.destroy',$project->id)}}" method="POST">
                     @csrf
                     @method('DELETE')
-                <button class="btn small btn-danger">Delete</button>
+                <button class="btn small btn-danger"><i class="fa-solid fa-trash"></i></button>
                 </form>
             </div>
         </div>  
